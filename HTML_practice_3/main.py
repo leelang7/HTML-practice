bfafb4d15a229ebede21755db3e5ff1d1e603783
Fixer <67309088+leelang7@users.noreply.github.com>
2025-02-12 10:17:57 +0900

    Add files via upload

---
+++ HTML_practice_3/main.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# 지시사항 1번을 작성하세요.
+
+    # 지시사항 2번을 작성하세요.
+    
+    # 지시사항 3번을 작성하세요.
+    
+    # 지시사항 4번을 작성하세요.
+    
---
+++ HTML_practice_4/main.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# 지시사항 1번을 작성하세요.
+
+    # 지시사항 2번을 작성하세요.
+    xpath = ''
+    print(xpath)
+
+    # 지시사항 3번을 작성하세요.
+    
+    # 지시사항 4번을 작성하세요.
+    xpath2 = ''
+    print(xpath2)
+
+    # 지시사항 5번을 작성하세요.
+    
+    # 지시사항 6번을 작성하세요.
+    xpath3 = ''
+    print(xpath3)
+
+    # 지시사항 7번을 작성하세요.
+    
